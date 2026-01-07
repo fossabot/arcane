607920e2f08e26747aa8aa795a7931205356aad9
Kyle Mendell <ksm@ofkm.us>
2026-01-07 13:30:59 -0600

    refactor: set diskUsagePath to /app/data/projects by default

--- backend/internal/services/settings_service.go
+++ backend/internal/services/settings_service.go
@@ -80,7 +80,7 @@ func (s *SettingsService) LoadDatabaseSettings(ctx context.Context) (err error) 
 func (s *SettingsService) getDefaultSettings() *models.Settings {
 	return &models.Settings{
 		ProjectsDirectory:          models.SettingVariable{Value: "/app/data/projects"},
-		DiskUsagePath:              models.SettingVariable{Value: "data/projects"},
+		DiskUsagePath:              models.SettingVariable{Value: "/app/data/projects"},
 		AutoUpdate:                 models.SettingVariable{Value: "false"},
 		AutoUpdateInterval:         models.SettingVariable{Value: "1440"},
 		PollingEnabled:             models.SettingVariable{Value: "true"},
